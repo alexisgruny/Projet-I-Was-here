3e68ce600c984d83c0079d67576369aa3a831341
alexisgruny <alexis.gruny60@gmail.com>
2024-05-23 16:48:31 +0200

    Optimisation -> Regex (Username/ password)

--- controller/modifyProfil.php
+++ controller/modifyProfil.php
@@ -6,8 +6,8 @@ require('../view/header.php');
 require('../view/navbar.php');
 
 // Déclaration des constantes pour les regex
-define('REGEX_USERNAME', '/^[0-9A-z]+$/');
-define('REGEX_PASSWORD', '/^[0-9A-z]+$/');
+define('REGEX_USERNAME', '/^[A-Za-z0-9_-]{3,20}$/');
+define('REGEX_PASSWORD', '/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/');
 define('REGEX_EMAIL', '/^[A-z0-9._%+-]+@[A-z0-9.-]+\.[A-z]{2,4}$/');
 
 $formErrorModify = array();
@@ -19,10 +19,10 @@ if (isset($_POST['modify'])) {
     // Vérification du mot de passe actuel
     if (password_verify($_POST['oldPassword'], $getPassword->password)) {
         // Validation des champs du formulaire
-        $fields = ['username', 'newPassword', 'passwordConfirm', 'email'];
-        foreach ($fields as $field) {
-            if (empty($_POST[$field])) {
-                $formErrorModify[$field] = 'Champs vide';
+        $form = ['username', 'newPassword', 'passwordConfirm', 'email'];
+        foreach ($form as $from) {
+            if (empty($_POST[$form])) {
+                $formErrorModify[$form] = 'Champs vide';
             }
         }
 
@@ -30,14 +30,14 @@ if (isset($_POST['modify'])) {
             // Validation des nouveaux mots de passe
             if ($_POST['newPassword'] == $_POST['passwordConfirm']) {
                 if (!preg_match(REGEX_PASSWORD, $_POST['newPassword'])) {
-                    $formErrorModify['newPassword'] = 'Le champ mot de passe est incorrect';
+                    $formErrorModify['newPassword'] = 'Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre, un caractère spécial et être d\'au moins 8 caractères.';
                 } else {
                     $modifyUser->password = password_hash($_POST['newPassword'], PASSWORD_DEFAULT);
                 }
 
                 // Validation du nom d'utilisateur
                 if (!preg_match(REGEX_USERNAME, $_POST['username'])) {
-                    $formErrorModify['username'] = 'Le champ username est incorrect';
+                    $formErrorModify['username'] = 'Le nom d\'utilisateur doit contenir entre 3 et 20 caractères et ne peut contenir que des lettres, des chiffres, des tirets et des underscores.';
                 } else {
                     $modifyUser->username = htmlspecialchars($_POST['username']);
                 }
